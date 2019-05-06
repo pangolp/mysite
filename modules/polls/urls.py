1090802b622a9907a7d49a7dc9e52c4f60d38dff
Pagani Walter <paganiwalter@hotmail.com>
2019-05-06 16:50:23 -0300

    agregando detalle a polls

--- modules/polls/urls.py
+++ modules/polls/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
 	IndexView,
+	PreguntaDetail,
 )
 
 
 app_name = 'polls'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('detalle/<int:pk>', PreguntaDetail.as_view(), name='detalle'),
 ]
--- modules/polls/views.py
+++ modules/polls/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from .models import Pregunta
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+	model = Pregunta
 	template_name = 'polls/index.html'
+	context_object_name = 'preguntas'
+
+
+class PreguntaDetail(DetailView):
+	model = Pregunta
+	template_name = 'polls/detalle.html'
+	context_object_name = 'pregunta'
