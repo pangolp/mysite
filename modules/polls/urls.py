580c2897910a09f405ca3db55422771df06c4c9f
Pagani Walter <paganiwalter@hotmail.com>
2019-05-07 16:55:04 -0300

    agregando el createView

--- modules/polls/urls.py
+++ modules/polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
 	IndexView,
 	PreguntaDetail,
+	PreguntaCreate,
 )
 
 
@@ -9,4 +10,5 @@ app_name = 'polls'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('detalle/<int:pk>', PreguntaDetail.as_view(), name='detalle'),
+    path('pregunta/create/', PreguntaCreate.as_view(), name='pregunta_create'),
 ]
--- modules/polls/views.py
+++ modules/polls/views.py
@@ -1,6 +1,8 @@
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Pregunta
 
 
@@ -14,3 +16,11 @@ class PreguntaDetail(DetailView):
 	model = Pregunta
 	template_name = 'polls/detalle.html'
 	context_object_name = 'pregunta'
+
+
+# Sirve para crear un formulario y dar de alta una Pregunta fuera del admin
+class PreguntaCreate(CreateView):
+	model = Pregunta
+	fields = ['titulo']
+	template_name = 'polls/pregunta/create.html'
+	success_url = reverse_lazy('polls:index')
