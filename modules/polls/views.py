7f2e1375eb504db9d6427e1743111172be1a8610
Pagani Walter <paganiwalter@gmail.com>
2019-05-15 19:07:01 -0300

    moviendo todos los ficheros html a polls

--- modules/polls/views.py
+++ modules/polls/views.py
@@ -8,13 +8,13 @@ from .models import Pregunta
 
 class IndexView(ListView):
 	model = Pregunta
-	template_name = 'polls/index.html'
+	template_name = 'polls/pregunta/index.html'
 	context_object_name = 'preguntas'
 
 
 class PreguntaDetail(DetailView):
 	model = Pregunta
-	template_name = 'polls/detalle.html'
+	template_name = 'polls/pregunta/detalle.html'
 	context_object_name = 'pregunta'
 
 
