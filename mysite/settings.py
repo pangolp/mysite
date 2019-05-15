7447931d864921b369efd5d7e12ea4a77c9c5cf3
Pagani Walter <paganiwalter@gmail.com>
2019-05-15 18:57:52 -0300

    creando template en polls

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'examples',
-        'USER': 'django',
-        'PASSWORD': 'django',
-#        'HOST': '',
-#        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'examples',
+#         'USER': 'django',
+#         'PASSWORD': 'django',
+# #        'HOST': '',
+# #        'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
