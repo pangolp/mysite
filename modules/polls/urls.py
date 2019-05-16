d030a8c33f2d5c8ee5cba3d04d9bc8eda0bd1302
Pagani Walter <paganiwalter@gmail.com>
2019-05-15 21:05:45 -0300

    modificando urls.py

--- modules/polls/urls.py
+++ modules/polls/urls.py
@@ -10,9 +10,29 @@ from .views import (
 
 app_name = 'polls'
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('detalle/<int:pk>', PreguntaDetail.as_view(), name='detalle'),
-    path('pregunta/create/', PreguntaCreate.as_view(), name='pregunta_create'),
-    path('pregunta/update/<int:pk>', PreguntaUpdate.as_view(), name='pregunta_update'),
-    path('pregunta/delete/<int:pk>', PreguntaDelete.as_view(), name='pregunta_delete'),
+    path(
+    	route='',
+    	view=IndexView.as_view(),
+    	name='index'
+    ),
+    path(
+    	route='detalle/<int:pk>',
+    	view=PreguntaDetail.as_view(),
+    	name='detalle'
+    ),
+    path(
+    	route='pregunta/create/',
+    	view=PreguntaCreate.as_view(),
+    	name='pregunta_create'
+    ),
+    path(
+    	route='pregunta/update/<int:pk>',
+    	view=PreguntaUpdate.as_view(),
+    	name='pregunta_update'
+    ),
+    path(
+    	route='pregunta/delete/<int:pk>',
+    	view=PreguntaDelete.as_view(),
+    	name='pregunta_delete'
+    ),
 ]
